9d9ea6945e9eb499d4916335017ad646d1ed1640
Raphael Golubev <raphael.golubev@gmail.com>
2025-12-18 21:07:11 +0300

    temp: added temporary pydantic-settings test in main func

--- main.py
+++ main.py
@@ -1,5 +1,35 @@
+# interfaceingame.com
+# CRT effect CSS / CRT scanline
+# gameuidatabase.com
+
+from src.settings import settings
+
+
 def main():
-    print("Hello from anomer-backend!!!")
+    app = settings.app.model_dump()
+    db = settings.db.model_dump()
+    mail = settings.mail.model_dump()
+    security = settings.security.model_dump()
+    redis = settings.redis.model_dump()
+    server = settings.server.model_dump()
+    
+    print("=== App")
+    print(app)
+
+    print("=== Database")
+    print(db)
+
+    print("=== Mail")
+    print(mail)
+
+    print("=== Security")
+    print(security)
+
+    print("=== Redis")
+    print(redis)
+
+    print("=== Server")
+    print(server)
 
 
 if __name__ == "__main__":
