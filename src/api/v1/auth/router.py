cc9834ab8a046c5ca8e2325ff0dd1972df9c6d94
Raphael Golubev <raphael.golubev@gmail.com>
2026-02-06 18:31:40 +0300

    feat: created basic login endpoint

--- src/api/v1/auth/router.py
+++ src/api/v1/auth/router.py
@@ -1,7 +1,10 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
 
-from src.security.hashing_encoding import hash_password
-from src.database.models.user import User
+from beanie.operators import Or
+
+from src.security.hashing_encoding import hash_password, verify_password
+from src.database.models.user import User, UserStatus
 import src.api.v1.auth.schemas as scheme
 
 
@@ -26,9 +29,38 @@ async def register_new_user(user: scheme.CreateUser):
 
 
 @router.post("/login")
-async def login_user():
+async def login_user(form_data: OAuth2PasswordRequestForm = Depends()):
     """ Аутентификация и авторизация пользователя """
-    pass
+    # ищем пользователя
+    user = await User.find_one(
+        Or(
+            User.email == form_data.username,
+            User.name == form_data.username
+        )
+    )
+    # проверяем логин и пароль
+    if not user or not verify_password(form_data.password, user.password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Неверный логин или пароль",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    # проверяем статус        
+    if user.status == UserStatus.created:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Вы не подтвердили адрес электронной почты",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    elif user.status == UserStatus.banned:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Да ты забанен епта",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    else:
+    # если все ок, генерируем токен
+        return {"status": "ok"}
 
 
 @router.post("/reset_password")
